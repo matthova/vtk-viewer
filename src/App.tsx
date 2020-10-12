81acd7eb305209570017e5cf5e5288e6e73369cf
Matt Hova <matthova87@gmail.com>
2020-10-11 20:31:56 -0700

    Rendering a model

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DesignSystemProvider } from './design-system';
+import { VTKRenderer } from './vtk/VTKRenderer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DesignSystemProvider>
+      <div>hello world</div>
+      <VTKRenderer />
+    </DesignSystemProvider>
   );
 }
 
